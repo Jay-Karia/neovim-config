f8e65ed41b984bec06df4e234ffcfed2653ef964
Jay <jay.sanjay.karia@gmail.com>
2024-12-31 19:55:05 +0530

    add dracula theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme('catppuccin-mocha')
+    end,
+  },
+  {
+    'Mofiqul/dracula.nvim',
+    config = function()
+      vim.cmd.colorscheme('dracula')
+    end
+  },
 }
